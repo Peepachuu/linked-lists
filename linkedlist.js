9551c2cb3621f29bc457bac61de1d9a812b5db81
Zafir <zafirhasan2006@gmail.com>
2022-09-26 14:23:51 +0400

    Set up functions that are needed for the linked lists

---
+++ linkedlist.js
@@ -0,0 +1,49 @@
+const LinkedList = function() {
+    function append(value) {
+
+    }
+
+    function prepend(value) {
+
+    }
+
+    function size() {
+
+    }
+
+    function head() {
+
+    }
+
+    function tail() {
+
+    }
+
+    function at(index) {
+
+    }
+
+    function pop() {
+
+    }
+
+    function contains(value) {
+
+    }
+
+    function find(value) {
+
+    }
+
+    function toString() {
+
+    }
+
+};
+
+const Node = function(value, nextNode) {
+    return {
+        value,
+        nextNode
+    };
+};
